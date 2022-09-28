0c9903616b624b463feb4853eaa8fbf6773c657d
Joshua Edgcombe <joshedgcombe@gmail.com>
2022-09-28 18:20:55 -0400

    Switch to using react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactGA from "react-ga4"
-// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import { useContext } from 'react'
 import { ThemeContext } from './contexts/theme'
@@ -8,15 +8,15 @@ import Header from './components/Header/Header'
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 import Footer from './components/Footer/Footer'
 
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/Skills'
+// import About from './components/About/About'
+// import Contact from './components/Contact/Contact'
+// import Projects from './components/Projects/Projects'
+// import Skills from './components/Skills/Skills'
 
 
-// import HomePage from './pages/home/Home'
-// import BlogPage from './pages/blog/Blog'
-// import ProjectsPage from './pages/projects/Projects'
+import HomePage from './pages/home/Home'
+import BlogPage from './pages/blog/Blog'
+import ProjectsPage from './pages/projects/Projects'
 
 import './App.css'
 
@@ -31,25 +31,29 @@ const App = () => {
           <Header />
 
           <main>
-      <About />
-      <Contact />
-      <Projects />
-      <Skills />
-      </main>
+            <Router>
+              <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='/blog' element={<BlogPage />} />
+                <Route path='/projects' element={<ProjectsPage />} />
+              </Routes>
+            </Router>
+          </main>
+
+
+
           <ScrollToTop />
           <Footer />
         </div>
   )
 }
 
-          //   <Router>
-          //     <Routes>
-          //       <Route path='/' element={<HomePage />} />
-          //       <Route path='/blog' element={<BlogPage />} />
-          //       <Route path='/projects' element={<ProjectsPage />} />
-          //     </Routes>
-          //   </Router>
-          // </main>
+      // <main>
+      // <About />
+      // <Contact />
+      // <Projects />
+      // <Skills />
+      // </main>
 
 
 
