d9cc8cc93c5a0780236809b8dca22a8d7509c078
Joshua Edgcombe <joshedgcombe@gmail.com>
2022-09-28 19:48:39 -0400

    Add preliminary blog post pages

---
+++ src/pages/blog/posts/2018-10-20-jekyll-revamp/JekyllRevampPost.js
@@ -0,0 +1,13 @@
+
+const JekyllRevampPost = () => (
+    <section id='Blog_Article' className='section blogposts'>
+      <h3 className='section__title'>Jekyll Revamp</h3>
+      <p>This weekend I decided to do a quick revamp of my personal website. Lately, I&apos;ve had the desire to write up some informational articles about some of the topics I&apos;ve been learning about in school as well as some of the insights I&apos;ve gained from project work.</p>
+
+      <p>In redoing my website I wanted a website with static pages that could be hosted using github pages to reduce costs while I&apos;m in school. I also wanted a means of posting blog articles to help solidify my knowledge through teaching, allow others to question my knowledge(further solidifying it), and provide another source of knowledge for those interested in the fields of software and electrical engineering. This led me creating to this website using the ruby based static website generator <a href='https://jekyllrb.com/'>Jekyll</a>.</p>
+
+      <p>This is my first attempt at using Jekyll and I have to say I&apos;m pleasantly surprised. I&apos;ve done a signficant amount of programming using python and have grown fond of <a href='https://github.com/pyenv/pyenv'>pyenv</a> for python version management. Seeing as how pyenv is a derivative of <a href='https://github.com/rbenv/rbenv'>rbenv</a>, I decided to use rbenv to handle ruby version management for this project. In the interest of sharing what I learned along the way, I am writing a quick <a href='/blog/jekyll-tutorial/'>Jekyll tutorial article</a> for anyone interested in giving Jekyll a shot in their own time.</p>
+    </section>
+)
+
+export default JekyllRevampPost
---
+++ src/pages/blog/posts/2018-10-23-jekyll-tutorial/JekyllTutorialPost.js
@@ -0,0 +1,9 @@
+import ReactMarkdown from 'react-markdown'
+
+const JekyllTutorialPost = () => (
+    <section id='Blog_Article' className='section blogposts'>
+    <ReactMarkdown># Hello *World*!</ReactMarkdown>
+    </section>
+)
+
+export default JekyllTutorialPost
