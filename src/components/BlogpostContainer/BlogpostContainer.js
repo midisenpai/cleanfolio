69591730c02973d18c611552cfeae8ea4a6e0798
Joshua Edgcombe <joshedgcombe@gmail.com>
2022-10-01 11:48:32 -0400

    Write better brief summaries for projects

--- src/components/BlogpostContainer/BlogpostContainer.js
+++ src/components/BlogpostContainer/BlogpostContainer.js
@@ -1,11 +1,9 @@
 import uniqid from 'uniqid'
-// import GitHubIcon from '@material-ui/icons/GitHub'
-// import LaunchIcon from '@material-ui/icons/Launch'
 import '../../App.css'
 
 const BlogpostContainer = ({ post }) => (
   <div className='blogpost'>
-    <h3><a href={post.permalink} className="link link--nav">{post.title}</a></h3>
+    <h3><a href={post.permalink} className="link">{post.title}</a></h3>
 
     <p className='project__description'>{post.body}</p>
     {post.stack && (
