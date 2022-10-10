56e433371d632ba10ba73f7a66f9b01387e237cb
Joshua Edgcombe <joshedgcombe@gmail.com>
2022-10-09 20:39:05 -0400

    Remove .btn styles from skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,15 +2,18 @@ import uniqid from 'uniqid'
 import { skills } from '../../portfolio'
 import './Skills.css'
 
+// <li key={uniqid()} className='skills__list-item btn btn--plain'>
+
 const Skills = () => {
   if (!skills.length) return null
+
 
   return (
     <section className='section skills' id='skills'>
       <h2 className='section__title'>Languages</h2>
       <ul className='skills__list'>
         {skills.map((skill) => (
-          <li key={uniqid()} className='skills__list-item btn btn--plain'>
+          <li key={uniqid()} className='skills__list-item'>
             {skill}
           </li>
         ))}
